99053587db7c95db7bfd4f7f55c1aba50a3a041c
Genna Walsh <gennawalsh@gmail.com>
2018-09-28 15:01:29 +0100

    add film class, tested

--- console.rb
+++ console.rb
@@ -4,17 +4,40 @@ require_relative("models/ticket.rb")
 require("pry")
 
 cust1 = Customer.new({
-  "name"  =>  "Bob",
+  "name"  =>  "Harvey",
   "funds" =>  500
   })
 cust2 = Customer.new({
-  "name"  =>  "Sally",
+  "name"  =>  "Fred",
   "funds" =>  700
   })
 cust3 = Customer.new({
   "name"  =>  "Genie",
   "funds" =>  200
   })
+
+# cust1.save()
+# cust2.save()
+# cust3.save()
+
+film1 = Film.new({
+  "title"   =>  "Watchmen",
+  "price"   =>  6.50
+  })
+
+film2 = Film.new({
+  "title"   =>  "Ocean's 8",
+  "price"   =>  7.25
+  })
+
+film3 = Film.new({
+  "title"   =>  "Ray",
+  "price"   =>  9.00
+  })
+
+# film1.save()  
+# film2.save()
+# film3.save()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -1,2 +1,14 @@
 require("pg")
 require_relative("../db/sql_runner.rb")
+
+class Film
+
+  attr_accessor :id, :title, :price
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @price = options["price"].to_f
+  end
+
+end
